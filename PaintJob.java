fd215af6a0d114ba600160c7c292f2b68cf14d26
Devon <115631814+robdvon@users.noreply.github.com>
2023-02-05 22:35:00 -0600

    Add files via upload

---
+++ PaintJob.java
@@ -0,0 +1,29 @@
+public class PaintJob {
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+
+        if(width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0){
+            return -1;
+        }
+        int numOfBuckets = (int) Math.ceil((width * height) / areaPerBucket);
+        numOfBuckets -= extraBuckets;
+        return numOfBuckets;
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket){
+
+        if(width <= 0 || height <= 0 || areaPerBucket <= 0){
+            return -1;
+        }
+
+        return (int) Math.ceil((width * height) / areaPerBucket);
+
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket){
+
+        if(area <= 0 || areaPerBucket <= 0){
+            return -1;
+        }
+        return (int) Math.ceil(area / areaPerBucket);
+    }
+}
