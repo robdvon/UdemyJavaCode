37594499ae8ec6e4b7fc3bc91d594af869afe419
Devon <115631814+robdvon@users.noreply.github.com>
2023-02-06 03:49:12 -0600

    Add files via upload

---
+++ RecordProject.java
@@ -0,0 +1,30 @@
+public class RecordProject {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            LPAStudent s = new LPAStudent("S92300" + i,
+                    switch (i) {
+                        case 1 -> "Mary";
+                        case 2 -> "Carol";
+                        case 3 -> "Tim";
+                        case 4 -> "Harry";
+                        case 5 -> "Lisa";
+                        default -> "Anonymous";
+                    },
+                    "05/11/1985",
+                    "Java masterclass");
+            System.out.println(s);
+        }
+        Student pojoStudent = new Student("S923006", "Ann", "05/11/1985", "Java Masterclass");
+        LPAStudent recordStudent = new LPAStudent("S923007", "Bill", "05/1/1985", "Java Masterclass");
+
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        pojoStudent.setClassList(pojoStudent.getClassList() + ", Java OCP Exam 829");
+//        recordStudent.setClassList(recordStudent.getClassList() + ", Java OCP Exam 829");
+
+        System.out.println(pojoStudent.getName() + " is taking "+ pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is taking "+ recordStudent.classList());
+    }
+}
