69ac5eb2d047516d142bfabbf90ae3b06efa2a44
Devon <115631814+robdvon@users.noreply.github.com>
2023-02-06 21:59:59 -0600

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -27,3 +27,147 @@ public class Inheritance {
         System.out.println("_____");
     }
 }
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    public class Animal {
+
+    protected String type;
+    private String size;
+    private double weight;
+
+    public Animal() {
+
+
+    }
+    public Animal(String type, String size, double weight) {
+        this.type = type;
+        this.size = size;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "type='" + type + '\'' +
+                ", size='" + size + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+
+    public void move(String speed){
+        System.out.println(type+ " moves " +speed);
+    }
+
+    public void makeNoise() {
+        System.out.println(type+ " makes some kind of noise");
+    }
+}
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+    public class Dog extends Animal {
+
+    private String earShape;
+    private String tailShape;
+
+    public Dog() {
+        super("Mutt", "Big", 50);
+    }
+
+    public Dog(String type, double weight) {
+        this(type, weight, "Perky", "Curled");
+    }
+    public Dog(String type, double weight, String earShape, String tailShape) {
+        super(type, weight < 15 ? "small" : (weight < 35 ? "medium" : "large"), weight);
+        this.earShape = earShape;
+        this.tailShape = tailShape;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "earShape='" + earShape + '\'' +
+                ", tailShape='" + tailShape + '\'' +
+                "} " + super.toString();
+    }
+
+    public void makeNoise() {
+
+        if (type == "Wolf") {
+            System.out.println("Ow Woo!");
+        }
+        bark();
+        System.out.println();
+    }
+
+    @Override
+    public void move(String speed) {
+        super.move(speed);
+//        System.out.println("Dogs walk, run, and wag their tail");
+        if (speed == "slow") {
+            walk();
+            wagTail();
+        } else {
+            run();
+            bark();
+        }
+        System.out.println();
+    }
+
+    private void bark()
+    {
+        System.out.println("Woof! ");
+    }
+    private void run()
+    {
+        System.out.println("Dog running ");
+    }
+    private void walk()
+    {
+        System.out.println("Dog walking ");
+    }
+    private void wagTail()
+    {
+        System.out.println("Tail wagging ");
+    }
+}
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+public class Fish extends Animal {
+
+    private int gills;
+    private int fins;
+
+    public Fish(String type, double weight, int gills, int fins) {
+        super(type, "small", weight);
+        this.gills = gills;
+        this.fins = fins;
+    }
+
+    private void moveMuscles() {
+        System.out.println("muscles moving ");
+    }
+    private void moveBackFin() {
+        System.out.println("backfin moving ");
+    }
+
+    @Override
+    public void move(String speed) {
+        super.move(speed);
+        moveMuscles();
+        if (speed == "fast") {
+            moveBackFin();
+        }
+        System.out.println();
+    }
+
+    @Override
+    public String toString() {
+        return "Fish{" +
+                "gills=" + gills +
+                ", fins=" + fins +
+                "} " + super.toString();
+    }
+}
+
+
+
